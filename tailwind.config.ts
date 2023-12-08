cce972d41b827ea8df73d01753824c820e838bdb
Sam Indiazi <saindiazi@gmail.com>
2023-12-07 22:19:18 -0600

    Init main

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +17,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 export default config
